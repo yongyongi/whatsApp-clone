89c441acd615740804f10c885fb59b697700371d
Yongyong <yongs8425@gmail.com>
2021-08-08 23:49:03 +0900

    google auth, redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Login from "./Login";
+import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import { useStateValue } from "./StateProvider";
 function App() {
+  // const [user, setUser] = useState(null);
+  const [{ user }, dispatch] = useStateValue();
   return (
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Sidebar />
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <Chat />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
